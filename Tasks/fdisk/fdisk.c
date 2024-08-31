f722b921762ef41592942218843c505f7e8108aa
Youssef <youssefosama789456@gmail.com>
2024-09-01 01:49:03 +0300

    Support Parsing GPT

--- Tasks/fdisk/fdisk.c
+++ Tasks/fdisk/fdisk.c
@@ -9,10 +9,9 @@ int main(int argc , char**argv){
 	ssize_t read_size;
 	PartitionEntry *partition_entry_ptr;
 	PartitionEntry *extended_partition_ptr = NULL;
-	PartitionEntry *next;
+	GPTPartitionEntry *gpt_ptr;
 	uint64_t absolute_lba;
 	int i = 0;
-	off_t lseek_offset;
 	uint64_t offset;
 	
 	if(2 != argc){
@@ -34,36 +33,80 @@ int main(int argc , char**argv){
 		return -1;
 	}
 	
-	partition_entry_ptr = (PartitionEntry *)&buff[446];	
-	printf("\033[1m%-10s %-10s %-10s %-10s %-10s %-10s %-10s %s\033[0m\n","Device","Boot","Start","End","Sectors","Size","Id","Type");
+	partition_entry_ptr = (PartitionEntry *)&buff[446];
 	
-	for(i=0 ;i<4; i++){
-		if(partition_entry_ptr[i].type == 0x00){
-			break;
+	/*Handle MBR */	
+	if(partition_entry_ptr[i].type != 0xee){
+		printf("\033[1m%-10s %-10s %-10s %-10s %-10s %-10s %-10s %s\033[0m\n","Device","Boot","Start","End","Sectors","Size","Id","Type");
+		
+		for(i=0 ;i<4; i++){
+			if(partition_entry_ptr[i].type == 0x00 || partition_entry_ptr[i].type == 0xee ){
+				break;
+			}
+			
+			printf("%s%i %-11c %-10u %-10u %-10u %u%s %10x",
+			argv[1],i+1,
+			partition_entry_ptr[i].status == 0x80? '*':' ',
+			partition_entry_ptr[i].lba,
+			partition_entry_ptr[i].lba + partition_entry_ptr[i].sector_count - 1,
+			partition_entry_ptr[i].sector_count,
+			(uint32_t)(((uint64_t)partition_entry_ptr[i].sector_count * 512)/(1024 * 1024 * 1024)),
+			"G",
+			partition_entry_ptr[i].type
+			);
+			
+			if( partition_entry_ptr[i].type == 0x83){
+				printf("%14s","Linux\n");
+			}
+			else if( partition_entry_ptr[i].type == 0x05){
+				printf("%14s","Extended\n");
+				extended_partition_ptr = (partition_entry_ptr+i);
+			}
+			else
+				printf("\n");
+			
+		}
+	}	
+	/*Support GPT*/
+	if(partition_entry_ptr[i].type == 0xee){
+		i=0;
+		printf("\033[1m%-10s %-10s %-10s %-10s %-10s %s\033[0m\n","Device","Start","End","Sectors","Size","Type");
+		offset = (uint64_t) 2 * 512;             //skip first 2 blocks
+		offset = lseek(fd , offset , SEEK_SET);
+		
+		if(-1 == offset){
+			perror("lseek ");
+			return -1;
 		}
 		
-		printf("%s%i %-11c %-10u %-10u %-10u %u%s %10x",
-		argv[1],i+1,
-		partition_entry_ptr[i].status == 0x80? '*':' ',
-		partition_entry_ptr[i].lba,
-		partition_entry_ptr[i].lba + partition_entry_ptr[i].sector_count - 1,
-		partition_entry_ptr[i].sector_count,
-		(uint32_t)(((uint64_t)partition_entry_ptr[i].sector_count * 512)/(1024 * 1024 * 1024)),
-		"G",
-		partition_entry_ptr[i].type
-		);
-		
-		if( partition_entry_ptr[i].type == 0x83){
-			printf("%14s","Linux\n");
+		read_size = read(fd , buff2 , 128);
+	
+		if(read_size <= 0){
+			perror("read ");
+			return -1;
 		}
-		else if( partition_entry_ptr[i].type == 0x05){
-			printf("%14s","Extended\n");
-			extended_partition_ptr = (partition_entry_ptr+i);
-		}
-		else
-			printf("\n");
 		
+		gpt_ptr = (GPTPartitionEntry *)&buff2[0];
+		while(gpt_ptr->first_lba != 0x0){
+			
+			printf("%s%i  %-10lu %-10lu %-10lu %lu\n",
+				argv[1],i+1,
+				gpt_ptr->first_lba,
+				gpt_ptr->last_lba,
+				gpt_ptr->last_lba - gpt_ptr->first_lba + 1,
+				(uint64_t)((gpt_ptr->last_lba - gpt_ptr->first_lba + 1)*512)/(1024*1024*1024)
+			);
+			read_size = read(fd , buff2 , 128);
+	
+			if(read_size <= 0){
+				perror("read ");
+				return -1;
+			}
+			gpt_ptr = (GPTPartitionEntry *)&buff2[0];	
+			i++;
+		}	
 	}
+	
 	
 	if(NULL != extended_partition_ptr){
 		
@@ -95,6 +138,7 @@ int main(int argc , char**argv){
 			"G",
 			partition_entry_ptr->type
 		);
+		
 		if( partition_entry_ptr->type == 0x83){
 			printf("%14s","Linux\n");
 		}
@@ -146,8 +190,7 @@ int main(int argc , char**argv){
 				
 			absolute_lba = 	extended_partition_ptr->lba;
 
-		}	
-			
+		}			
 	}
 	
 	close(fd);
--- Tasks/fdisk/fdisk.h
+++ Tasks/fdisk/fdisk.h
@@ -1,3 +1,6 @@
+#ifndef FDISK_H
+#define FDISK_H 
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -21,3 +24,16 @@ typedef struct{
 	uint32_t sector_count; 
 
 }PartitionEntry;
+
+typedef struct {
+    uint8_t  partition_type_guid[16];
+    uint8_t  unique_partition_guid[16];
+    uint64_t first_lba;
+    uint64_t last_lba;
+    uint64_t attributes;
+    uint16_t partition_name[36];
+}GPTPartitionEntry;
+
+
+
+#endif
